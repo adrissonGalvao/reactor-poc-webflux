da77a1f462f719292ae372e41d8c7603d9e78e02
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-08-14 23:34:41 -0300

    chore: init

---
+++ src/main/java/com/edvmorango/reactorpoc/ReactorPocApplication.java
@@ -0,0 +1,12 @@
+package com.edvmorango.reactorpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorPocApplication.class, args);
+	}
+}
